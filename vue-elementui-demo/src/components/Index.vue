bbb43b5bb8e2e6bb2b8784045edd8fb83dd83b83
少停 <510738319@qq.com>
2021-01-23 17:55:59 +0800

    echarts
    echarts

--- vue-elementui-demo/src/components/Index.vue
+++ vue-elementui-demo/src/components/Index.vue
@@ -8,7 +8,7 @@
                 <span slot="title">导航一</span>
               </template>
               <el-menu-item-group>
-                <el-menu-item index="1-1">选项1</el-menu-item>
+                <el-menu-item index="/index">选项1</el-menu-item>
                 <el-menu-item index="1-2">选项2</el-menu-item>
               </el-menu-item-group>
             </el-submenu>
--- vue-elementui-demo/src/main.js
+++ vue-elementui-demo/src/main.js
@@ -6,8 +6,8 @@ import router from './router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 
-Vue.config.productionTip = false
 Vue.use(ElementUI)
+Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
